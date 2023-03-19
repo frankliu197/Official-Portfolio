f3c5130577dc3ddb66f640b03a89257d678ae0d7
OlakunleS <Kennylanre9@gmail.com>
2023-03-18 21:39:49 -0400

    Update 3182023.2

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
     transpileDependencies: [
         'vuetify'
     ],
+    publicPath: process.env.NODE_ENV === "production" ? "/Official-Portfolio/" : "/",
 
     pluginOptions: {
         i18n: {
